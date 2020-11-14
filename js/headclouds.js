a2e69e71c0293a7b1d0ad39935ecf91050b3d783
Zorai Naccal <46486624+Naccal@users.noreply.github.com>
2020-11-14 14:36:17 +0000

    Beginning Test
    Pommy Ipsum, a test to see how the website looks.

---
+++ js/headclouds.js
@@ -0,0 +1,6 @@
+document.write('\
+<div id="clouds"><IMG STYLE="position:absolute; TOP:20px; RIGHT:20px; WIDTH:300; HEIGHT:300px; pointer-events: none; selectable="false;" SRC="images/Sunparts.png" ">
+<IMG STYLE="position:absolute; TOP:0px; LEFT:50%; WIDTH:40; HEIGHT:40px; pointer-events: none; selectable="false;" SRC="images/hh.png" "><div class="cloud x1"></div><div class="cloud x2"></div>	<div class="cloud x3"></div>	<div class="cloud x4"></div>	<div class="cloud x5"></div>
+</div>
+\
+');
